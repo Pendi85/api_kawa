9ce8d815a2a585fb85a4b323e07c6b9eb9e66f23
Pendi85 <81863481+Pendi85@users.noreply.github.com>
2023-03-08 16:01:06 +0100

    :sparkles: Développement des fonctions CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,9 +1,11 @@
+import { async } from 'regenerator-runtime';
 import { Op } from 'sequelize';
 import model from '../models';
 
 const { Product } = model;
 
 export default {
+
   async findAll(req, res) {
     Product.findAll()
     .then(data => {
@@ -12,7 +14,62 @@ export default {
     .catch(err => {
         res.status(500).send({
             message:
-                err.message || "Some error occurred while retrieving beers."
+                err.message || "Some errors occurred while retrieving products."
+        });
+    });
+  },
+
+  async createProduct(req, res) {
+    console.log(req);
+
+    Product.create({
+      name: req.body.name,
+      stock: req.body.stock,
+      price: req.body.price,
+      description: req.body.description,
+      color: req.body.color,
+      createAt: req.body.createAt,
+      modelRA: req.body.modelRA,
+      image: req.body.image
+    })
+    .then(data => {
+      res.status(201).send(data);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || `Some errors occurred while creating product '${req.body.name}'.`
+        });
+    });
+  },
+
+  async updateProduct(req, res) {
+    Product.update(req.body, {
+      where: { id: req.query.id }
+    })
+    .then(data => {
+      res.send(`Produit '${req.query.id}' mis à jour`);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || `Some errors occurred while updating product '${req.query.id}'.`
+        });
+    });
+  },
+
+  async deleteProduct(req, res) {
+
+    Product.destroy({
+      where: { id: req.query.id }
+    })
+    .then(() => {
+      res.send(`Produit '${req.query.id}' supprimé`);
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while deleting product '${req.query.id}'."
         });
     });
   }
