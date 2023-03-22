8fec5e818ee7abb2e9aa5f773678b775364e633e
Marin Tostivint <marin.tostivint@epsi.fr>
2023-03-22 20:01:09 +0100

    jwt token api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,9 @@
 import ProductController from '../controllers/ProductController'
+import AuthMiddleware from "../middleware/AuthMiddleware";
 
 export default (app) => {
-    app.get('/products', ProductController.findAll);
+    app.get('/products', AuthMiddleware.authenticateJWT, ProductController.findAll);
     app.post('/products', ProductController.createProduct);
     app.put('/products', ProductController.updateProduct);
-    app.delete('/products',  ProductController.deleteProduct);
-
-    // Create a catch-all route for testing the installation.
-    app.all('*', (req, res) => res.status(200).send({
-        message: 'Hello World!',
-    }));
+    app.delete('/products', ProductController.deleteProduct);
 };
